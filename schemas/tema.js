fd5726c6660e094defc3a75b2a8dcbcd1d61e2b0
Cristian Damil <cristiancode2023@gmail.com>
2024-06-28 17:17:34 +0200

    Fixed schema for tema

--- schemas/tema.js
+++ schemas/tema.js
@@ -2,7 +2,13 @@ const Joi = require("joi");
 
 const temaSchema = Joi.object({
   id_tema: Joi.number().min(0),
-  tema: Joi.string().pattern(new RegExp("^[\\p{L}0-9]{3,20}$", "u")).required(),
+  tema: Joi.string()
+    .pattern(new RegExp("^[\\p{L}0-9]{3,20}$", "u"))
+    .when("id_tema", {
+      is: Joi.exist(),
+      then: Joi.optional(),
+      otherwise: Joi.required(),
+    }),
 });
 
 module.exports = temaSchema;
